b60858254e5e943712c271bbf9f76a02209faf0e
Joshua Lyons <lyons_joshua@bah.com>
2022-12-21 20:39:05 -0600

    Adding in activeDirectory2 to make the LDAP calls async as well as to handle recursively calling to get the groups of groups.

--- index.js
+++ index.js
@@ -14,28 +14,29 @@ const SamlStrategy = require('passport-saml').Strategy;
 
 const ldap = require('ldapjs');
 const logger = require('advana-logger');
+const AD = require('activedirectory2').promiseWrapper;
 
 const SAML_CONFIGS = require('./samlConfigs');
 
 const SSO_DISABLED = process.env.DISABLE_SSO === 'true';
 
 const retry_strategy = (options) => {
-	if(options.attempt > 75){
+	if (options.attempt > 75) {
 		return new Error('Redis connection attempts timed out');
 	}
 
 	// square number of retries to get an exponential curve of retries
 	// return number of milleseconds to wait before retrying again
 	logger.info('Redis attempting to retry connection. Try number: ', options.attempt);
-	return options.attempt * options.attempt *100;
-}
-const client = redis.createClient({url: process.env.REDIS_URL, retry_strategy: retry_strategy});
+	return options.attempt * options.attempt * 100;
+};
+const client = redis.createClient({ url: process.env.REDIS_URL, retry_strategy: retry_strategy });
 
 const pool = new Pool({
 	user: process.env.PG_USER,
 	password: process.env.PG_PASSWORD,
 	host: process.env.PG_HOST,
-	database: process.env.PG_UM_DB
+	database: process.env.PG_UM_DB,
 });
 
 let keyFileData;
@@ -92,12 +93,12 @@ const getAllowedOriginMiddleware = (req, res, next) => {
 	} else {
 		next();
 	}
-}
+};
 const redisSession = () => {
 	let redisOptions = {
 		host: process.env.REDIS_URL,
 		port: '6379',
-		client: client
+		client: client,
 	};
 
 	let extraSessionOptions = {};
@@ -108,7 +109,7 @@ const redisSession = () => {
 
 	return session({
 		store: new RedisStore(redisOptions),
-		expires: new Date(Date.now() + (43200000)),
+		expires: new Date(Date.now() + 43200000),
 		secret: JSON.parse(process.env.EXPRESS_SESSION_SECRET),
 		resave: false,
 		saveUninitialized: true,
@@ -165,7 +166,10 @@ const fetchUserInfo = async (req) => {
 		let adUser = {};
 
 		if (process.env.AD_ENABLED === 'true') {
-			adUser = await fetchActiveDirectoryUserInfo(userid.split('@')[0]);
+			const t0 = new Date().getTime();
+			adUser = await fetchActiveDirectoryUserInfo(req?.user?.id);
+			const t1 = new Date().getTime();
+			console.log(`Call to fetchActiveDirectoryUserInfo took ${(t1 - t0) / 1000} seconds.`);
 		}
 
 		return {
@@ -176,6 +180,7 @@ const fetchUserInfo = async (req) => {
 			disabled: user.disabled || adUser.disabled,
 			cn: req?.user?.cn || adUser.cn,
 			email: req?.user?.email || adUser.email,
+			retrievedADPerms: adUser?.perms?.length > 0,
 		};
 	} catch (err) {
 		console.error(err);
@@ -185,125 +190,119 @@ const fetchUserInfo = async (req) => {
 	}
 };
 
-const fetchActiveDirectoryUserInfo = (userid) => {
-	return new Promise((resolve, reject) => {
-		const opts = {
-			filter: '(sAMAccountName=' + userid + ')',  //simple search
-			scope: 'sub'
-		};
-		let ldapclient;
-		try {
-			ldapclient = ldap.createClient({
-				url: process.env.LDAP_URL,
-				tlsOptions: {
-					rejectUnauthorized: false,
-					ca: [process.env.LDAP_CERT.replace(/\\n/g, '\n')]
-				}
-			});
-			
-		} catch (err) {
-			console.log('Error in create ' + err, 'EOF6GFC', userid);
-			resolve({});
-		}
-		//userid will be in a "##@mil" format
-		//userPrincipalName format: "123456789@drced.local"
-		//sAMAccountName format: "123456798"
+const fetchActiveDirectoryUserInfo = async (userId) => {
+	const config = {
+		url: process.env.LDAP_URL,
+		tlsOptions: {
+			rejectUnauthorized: false,
+			ca: [process.env.LDAP_CERT.replace(/\\n/g, '\n')],
+		},
+		baseDN: process.env.LDAP_USER_FOLDER_CN,
+		username: process.env.LDAP_USERNAME,
+		password: process.env.LDAP_PASSWORD,
+	};
 
-		/*bind use for authentication*/
-		ldapclient.bind(process.env.LDAP_USERNAME, process.env.LDAP_PASSWORD, function (err) {
-			if (err) {
-				console.log('Error in bind ' + err);
-				resolve({});
-			} else {
-				//user directory needs to be updated to match prod
-				ldapclient.search(process.env.LDAP_USER_FOLDER_CN, opts, function (err, res) {
-					if (err) {
-						console.log('Error in search ' + err);
-						resolve({});
-					} else {
-						res.on('searchEntry', function (entry) {
-							ldapclient.unbind();
-							const groupPerms = [];
-							const userObj = entry.object;
-							if (userObj.memberOf) {
-								for (let group of userObj.memberOf) {
-									const cnParse = group.split(",", 1)
-									const cnstring = cnParse[0].substring(3)
-									groupPerms.push(cnstring)
-								}
-							}
+	const ad = new AD(config);
 
-							resolve({
-								id: userObj.sAMAccountName,
-								displayName: userObj.displayName,
-								perms: groupPerms,
-								sandboxId: 1,
-								cn: userObj.cn,
-								dn: userObj.dn,
-								disabled: false,
-								email: userObj.mail,
-							});
-						});
-						res.on('error', function (err) {
-							ldapclient.unbind();
-							console.log('error: ' + err.message);
-							resolve({});
-						});
-						res.on('end', (result) => {
-							ldapclient.unbind();
-							resolve({});
-						});
-					}
-				});
-			}
+	const userObj = await ad.findUser(userId.split('@')[0]);
+	if (!userObj) {
+		console.log('User: ' + userId + ' not found.');
+		return {};
+	}
+
+	const groups = await ad.getGroupMembershipForUser(userId.split('@')[0]);
+	const groupPerms = [];
+	if (!groups) {
+		console.log('User: ' + userId + ' not found.');
+	} else {
+		groups.forEach((group) => {
+			groupPerms.push(group.cn);
 		});
-	});
-}
+	}
+
+	return {
+		id: userObj.sAMAccountName,
+		displayName: userObj.displayName,
+		perms: groupPerms,
+		sandboxId: 1,
+		cn: userObj.cn,
+		dn: userObj.dn,
+		disabled: false,
+		email: userObj.mail,
+	};
+};
+
+const fetchActiveDirectoryPermissions = async (userId) => {
+	const config = {
+		url: process.env.LDAP_URL,
+		tlsOptions: {
+			rejectUnauthorized: false,
+			ca: [process.env.LDAP_CERT.replace(/\\n/g, '\n')],
+		},
+		baseDN: process.env.LDAP_USER_FOLDER_CN,
+		username: process.env.LDAP_USERNAME,
+		password: process.env.LDAP_PASSWORD,
+	};
+
+	const ad = new AD(config);
+
+	const groups = await ad.getGroupMembershipForUser(userId.split('@')[0]);
+	const groupPerms = [];
+	if (!groups) {
+		console.log('User: ' + userId + ' not found.');
+	} else {
+		groups.forEach((group) => {
+			groupPerms.push(group.cn);
+		});
+	}
+
+	return groupPerms;
+};
 
 // THE CODE BELOW IS GENERAL FUNCTIONS TO ADD/REMOVE USERS TO AND FROM ACTIVE DIRECTORY GROUPS. UX DESIGN REQUIRED BEFORE FUCTIONS FINISHED
 /*use this to add user to group*/
 function addUserToGroup(groupname, userToAddDn) {
 	const ldapclient = ldap.createClient({
-		url: process.env.LDAP_URL
+		url: process.env.LDAP_URL,
 	});
-    const change = new ldap.Change({
-        operation: 'add',
-        modification: {
-            member:[userToAddDn]
-        }
-    });
+	const change = new ldap.Change({
+		operation: 'add',
+		modification: {
+			member: [userToAddDn],
+		},
+	});
 
-    ldapclient.modify(groupname, change, function (err) {
-        if (err) {
-            console.log("err in add user in a group " + err);
-        } else {
-            console.log("added user in a group")
-        }
-    });
+	ldapclient.modify(groupname, change, function (err) {
+		if (err) {
+			console.log('err in add user in a group ' + err);
+		} else {
+			console.log('added user in a group');
+		}
+	});
 }
 
 /*use this to remove user from group*/
 function removeUserFromGroup(groupname, userToRemoveDn) {
 	const ldapclient = ldap.createClient({
-		url: process.env.LDAP_URL
+		url: process.env.LDAP_URL,
 	});
-    const change = new ldap.Change({
-        operation: 'delete',
-        modification: {
-            member:[userToRemoveDn]
-        }
-    });
+	const change = new ldap.Change({
+		operation: 'delete',
+		modification: {
+			member: [userToRemoveDn],
+		},
+	});
 
-    ldapclient.modify(groupname, change, function (err) {
-        if (err) {
-            console.log("err in remove user from a group " + err);
-        } else {
-            console.log("removed user from a group")
-        }
-    });
+	ldapclient.modify(groupname, change, function (err) {
+		if (err) {
+			console.log('err in remove user from a group ' + err);
+		} else {
+			console.log('removed user from a group');
+		}
+	});
 }
 
-const hasPerm = (desiredPermission = "", permissions = []) => {
+const hasPerm = (desiredPermission = '', permissions = []) => {
 	if (permissions.length > 0) {
 		for (let perm of permissions) {
 			if (
@@ -320,10 +319,9 @@ const hasPerm = (desiredPermission = "", permissions = []) => {
 
 const permCheck = (req, res, next, permissionToCheckFor = []) => {
 	try {
-		let permissions = (req.session.user && req.session.user.perms) ? req.session.user.perms : [];
+		let permissions = req.session.user && req.session.user.perms ? req.session.user.perms : [];
 		for (let p of permissionToCheckFor) {
-			if (hasPerm(p, permissions))
-				return next();
+			if (hasPerm(p, permissions)) return next();
 		}
 	} catch (err) {
 		console.error('Error reading request permissions.');
@@ -353,12 +351,15 @@ const SSORedirect = (req, res) => {
 	} else {
 		return res.redirect('/');
 	}
-}
+};
 
 const setupSaml = (app) => {
-
-	passport.serializeUser((user, done) => { done(null, user); });
-	passport.deserializeUser((user, done) => { done(null, user); });
+	passport.serializeUser((user, done) => {
+		done(null, user);
+	});
+	passport.deserializeUser((user, done) => {
+		done(null, user);
+	});
 
 	passport.use(
 		new SamlStrategy(SAML_CONFIGS.SAML_OBJECT, function (profile, done) {
@@ -385,42 +386,47 @@ const setupSaml = (app) => {
 	app.use(passport.initialize());
 	app.use(passport.session());
 
-	app.get('/login', (req, res, next) => {
-		const referer = req.get('Referer');
-		if (referer) {
-			try {
-				const parsedReferer = new URL(referer);
-				const refererOrigin = parsedReferer.origin.replace('www.', '');
-				const approvedClients = process.env.APPROVED_API_CALLERS.split(' ');
-				// store referer origin in session in order to redirect to correct domain after SAML auth
-				if (approvedClients.includes(refererOrigin) && refererOrigin !== approvedClients[0]) {
-					req.session.AlternateSsoOrigin = refererOrigin;
+	app.get(
+		'/login',
+		(req, res, next) => {
+			const referer = req.get('Referer');
+			if (referer) {
+				try {
+					const parsedReferer = new URL(referer);
+					const refererOrigin = parsedReferer.origin.replace('www.', '');
+					const approvedClients = process.env.APPROVED_API_CALLERS.split(' ');
+					// store referer origin in session in order to redirect to correct domain after SAML auth
+					if (approvedClients.includes(refererOrigin) && refererOrigin !== approvedClients[0]) {
+						req.session.AlternateSsoOrigin = refererOrigin;
+					}
+				} catch (error) {
+					logger.info(error);
 				}
-			} catch (error) {
-				logger.info(error);
 			}
+			passport.authenticate('saml', { failureRedirect: '/login/fail' })(req, res, next);
+		},
+		(req, res) => {
+			SSORedirect(req, res);
 		}
-		passport.authenticate('saml', { failureRedirect: '/login/fail' })(req, res, next);
-	}, (req, res) => { SSORedirect(req, res); });
-
-	app.post('/login/callback',
-		passport.authenticate('saml', { failureRedirect: '/login/fail' }),
-		(_req, res) => { res.redirect('/api/setUserSession'); }
 	);
 
-	app.get('/login/fail',
-		(_req, res) => { res.status(401).send('Login failed'); }
+	app.post('/login/callback', passport.authenticate('saml', { failureRedirect: '/login/fail' }), (_req, res) => {
+		res.redirect('/api/setUserSession');
+	});
+
+	app.get('/login/fail', (_req, res) => {
+		res.status(401).send('Login failed');
+	});
+
+	app.get(
+		'/api/setUserSession',
+		(req, res, next) => {
+			if (req.isAuthenticated()) return next();
+			else return res.redirect('/login');
+		},
+		setUserSession
 	);
-
-
-	app.get('/api/setUserSession', (req, res, next) => {
-		if (req.isAuthenticated())
-			return next();
-		else
-			return res.redirect('/login');
-	}, setUserSession);
-
-}
+};
 
 // END SAML PORTION
 
@@ -432,5 +438,5 @@ module.exports = {
 	redisSession,
 	setUserSession,
 	setupSaml,
-	fetchActiveDirectoryUserInfo
+	fetchActiveDirectoryPermissions,
 };
